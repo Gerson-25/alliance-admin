e65d7cbf5bd05141b39856446ee02d876f4324db
Gerson-25 <45273906+Gerson-25@users.noreply.github.com>
2020-02-24 15:04:47 -0600

    tabs selection

--- js/app.js
+++ js/app.js
@@ -5,4 +5,29 @@ document.addEventListener('DOMContentLoaded', function() {
     instances.dropdownOptions
   });
 
+  
+
   console.log(document.getElementsByClassName('tabs'))
+
+  document.getElementById("list-promotions").style.display = "block"
+
+
+  function openTab(evt, TabId){
+  
+    var i, tabs, tablinks;
+  tabs = document.getElementsByClassName("tabs");
+  for (i = 0; i < tabs.length; i++) {
+    if(tabs[i]==2){
+      tabs[i].style.display = "active";
+    }
+    tabs[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+
+  document.getElementById(TabId).style.display = "block";
+    evt.currentTarget.className += "active";
+   }
